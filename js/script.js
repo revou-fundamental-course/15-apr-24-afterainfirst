e1b026acdb622bc4c2203f2c6847b6f75bff84e6
afterainfirst <afterain.real@gmail.com>
2024-04-23 20:51:13 +0700

    assets

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // ini file js
+function convertToCelsius() {
+    var inputCelsius = document.getElementById("inputCelsius").value;
+    var outputFahrenheit = (parseFloat(inputCelsius) * 9/5) + 32;
+    document.getElementById("outputFahrenheit").value = outputFahrenheit.toFixed(2);
+
+    document.getElementById("calculation").value = inputCelsius + " \u00B0C * 9/5 + 32 = " + outputFahrenheit.toFixed(2) + " \u00B0F";
+}
+
+function reset() {
+    document.getElementById("inputCelsius").value = "";
+    document.getElementById("outputFahrenheit").value = "";
+    document.getElementById("calculation").value = "";
+}
+
+function reverseConversion() {
+    var inputFahrenheit = document.getElementById("outputFahrenheit").value;
+    var outputCelsius = (parseFloat(inputFahrenheit) - 32) * 5/9;
+    document.getElementById("inputCelsius").value = outputCelsius.toFixed(2);
+
+    document.getElementById("calculation").value = "(" + inputFahrenheit + " \u00B0F - 32) * 5/9 = " + outputCelsius.toFixed(2) + " \u00B0C";
+}
